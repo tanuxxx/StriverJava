ae021cb6d45bee5c02ae7074b74aab6f9713a0b4
tanuxxx <tanmaymp92@gmail.com>
2025-09-17 14:38:18 +0530

    gpt help

--- src/Array/A7_Move_Zero_to_End.java
+++ src/Array/A7_Move_Zero_to_End.java
@@ -4,42 +4,41 @@ import java.util.Scanner;
 
 public class A7_Move_Zero_to_End {
 
-    public static void Movezeros(int [] arr){
+    public static void MoveZeroes(int [] arr){
         int n = arr.length;
-        int temp = 0;
-            for(int i = 0; i<n ; i++){
-            if(arr[i] != 0){
+        int temp = 0; // position for non-zero elements
+
+        // Step 1: Move all non-zero elements to the front
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
                 arr[temp] = arr[i];
                 temp++;
             }
+        }
 
-            while(temp <n){
-                arr[temp] = 0;
-                temp++;
-            }
+        // Step 2: Fill remaining positions with zero
+        while (temp < n) {
+            arr[temp] = 0;
+            temp++;
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Array size= ");
+        int n = sc.nextInt();
 
-        int choice = 1;
-        while (choice == 1) {
-            System.out.println("Array size: ");
-            int n = sc.nextInt();
-
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-
-            Movezeros(arr);
-            for(int num : arr){
-                System.out.println(num+" ");
-            }
-
-
-            System.out.println("enter 1 to contunue");
-            choice = sc.nextInt();
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
         }
+
+
+        MoveZeroes(arr);
+
+        System.out.println("\nAfter moving zeroes:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
     }
 }
